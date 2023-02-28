75325ec859d6617d3abea7261fefe759dbcb5bd2
BarZ <andy820621@gmail.com>
2023-03-01 00:01:24 +0900

    add vite with vue & scss

---
+++ vue_vite_scss_init/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { reactive, ref } from "vue";
+</script>
+
+<template></template>
+
+<style lang="scss" scoped>
+@mixin size($width, $height: $width) {
+	width: $width;
+	height: $height;
+}
+</style>
